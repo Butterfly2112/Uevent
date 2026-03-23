b61ef7fbe6fa027f9586f8dcf7e94f63571319ee
DianaMalashta <dianamalasta@gmail.com>
2026-03-23 13:20:46 +0200

    Fixed confirm email letter

--- server/src/email/email.service.ts
+++ server/src/email/email.service.ts
@@ -22,9 +22,10 @@ export class EmailService {
     token: string,
   ): Promise<void> {
     const host = this.configService.get('HOST_FOR_EMAIL');
+    const port = this.configService.get('PORT_FOR_EMAIL');
     const url = host.startsWith('http')
       ? `${host}/confirm-email?token=${token}`
-      : `http://${host}/confirm-email?token=${token}`;
+      : `http://${host}:${port}/confirm-email?token=${token}`;
     const info = await this.transporter.sendMail({
       from: `"Uevent" <${this.configService.get('SMTP_USER')}>`,
       to: email,
